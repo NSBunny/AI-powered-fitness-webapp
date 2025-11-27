daedb0f7f81d5c1bef19c95c564d4b014cda4a77
NS Bunny <116730668+NSBunny@users.noreply.github.com>
2025-11-27 10:02:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Allow requests from this origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+        credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    })
+);
 app.use(express.json());
 
 // Database Connection
